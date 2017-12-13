b46fe75ca2868bac4c887e128ca744ade429682e
uttamchoudhary <uttampratapchoudhary@gmail.com>
2017-12-13 17:44:14 +0530

    Build error fixes

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from './carousel/carousel.module';
+export * from './carousel/carousel.component';
+export * from './carousel/model/settings.model';
+export * from './carousel/model/image.model';
